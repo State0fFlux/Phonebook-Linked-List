088b98f6f02c35c3ebbac7c7f3576787df020173
State0fFlux <127552262+State0fFlux@users.noreply.github.com>
2023-03-10 13:48:53 -0800

    Update ListNode.java

--- ListNode.java
+++ ListNode.java
@@ -1,12 +1,11 @@
-// Brady Manske
-// 2/10/23
-// CS&145
-// Assignment 2: Phone Book (file 1 of 3)
 
-// This defines a class of ListNodes that
-// store contact information within nodes,
-// and are capable of forming a linked list resembling a phonebook of contacts
-
+/*
+ * This defines a class of ListNodes that
+ * store contact information within nodes,
+ * and are capable of forming a linked list resembling a phonebook of contacts.
+ * @author Brady Manske
+ * @version 2023/01/31
+ */
 public class ListNode {
     // declaring fields
     public String firstName;
